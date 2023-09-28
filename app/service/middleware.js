87e809677530bd63c7ec6d71e87279dc4f1c0659
SixTanDev <sixtandev@gmail.com>
2023-09-28 02:05:52 -0500

    feat: Add middleware to access resources

--- app/service/middleware.js
+++ app/service/middleware.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 
 const bcryptSaltPasawword = bcrypt.genSaltSync(10);
 const bcryptSaltToken = bcrypt.genSaltSync(20);
@@ -13,7 +15,33 @@ function attachBcryptSaltToken(req, res, next) {
   next();
 }
 
+function accessResource(req, res, next) {
+  const token = req.headers.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'An authentication token was not provided' });
+  }
+
+  jwt.verify(token, bcryptSaltToken, (err, decoded) => {
+    console.log(err, decoded)
+    if (err) {
+      return res.status(401).json({ message: 'Invalid authentication token' });
+    }
+
+    if (decoded.user[0].user_type_id !== 1){
+      return res.status(403).json({
+        message: 'The user does not have access to the resource',
+        detail: 'Access only for administrator',
+      });
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
+
 module.exports = {
   attachBcryptSaltPassword,
   attachBcryptSaltToken,
+  accessResource
 };
