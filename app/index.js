d6ffde99294c97b964b136d0dd77d6ddead6a3cd
SixTanDev <sixtandev@gmail.com>
2023-09-28 02:26:51 -0500

    feat: "Refactor app configuration"

--- app/index.js
+++ app/index.js
@@ -4,11 +4,13 @@ const app = express();
 const routes = require("./routes");
 
 const UserService = require("./service/users");
+const ProductSerive = require("./service/products")
 
 module.exports = (config) => {
   const log = config.log();
 
   const userService = new UserService();
+  const productService = new ProductSerive();
   // Add a request logging middleware in development mode
   if (app.get("env") === "development") {
     app.use((req, res, next) => {
@@ -19,7 +21,7 @@ module.exports = (config) => {
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use("/", routes({ userService }));
+  app.use("/", routes({ userService, productService }));
 
   return app;
 };
